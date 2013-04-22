49e0cfae65d8f66a4fcf908ddd6287f340f8dddd
Nick Recobra <oruen@undev.ru>
2013-04-22 19:58:14 +0400

    All long numbers considered refs.

--- assets/javascripts/helpers/resulut.js
+++ assets/javascripts/helpers/resulut.js
@@ -2,7 +2,9 @@ Em.Handlebars.registerBoundHelper("summary", function(obj) {
   var res = [];
   for (i in obj) {
     if (obj.hasOwnProperty(i)) {
-      res.push("<strong>" + i + "</strong>: " + JSON.stringify(obj[i]));
+      var str = JSON.stringify(obj[i]);
+      str = str.replace(/(\d+){4,}/g, function(m) {return "<a href=\"#/object/" + m + "\">" + m + "</a>"})
+      res.push("<strong>" + i + "</strong>: " + str);
     }
   }
   return new Handlebars.SafeString(res.join("<br>"));
